dd8f8063a2c18c46b63c46bb471c3196cfde57f1
Ejz9 <114949726+Ejz9@users.noreply.github.com>
2024-11-06 14:03:40 -0600

    Initial Commit.

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Optional: Add some basic styling */
+nav {
+  margin-bottom: 10px;
+}
+router-link {
+  margin-right: 10px;
+}
+</style>
